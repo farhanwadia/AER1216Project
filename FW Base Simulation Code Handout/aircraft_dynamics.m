0bc7a2954db9ebf2c1d9b8a449a968945cf619f9
Farhan <78187115+farhanwadia@users.noreply.github.com>
2021-12-14 21:31:09 -0500

    Added David's latest fixed wing code

--- FW Base Simulation Code Handout/aircraft_dynamics.m
+++ FW Base Simulation Code Handout/aircraft_dynamics.m
@@ -254,82 +254,20 @@ end
 function Derivatives(block)
 
 % Rename parameters
-
 P = block.DialogPrm(1).Data; % must duplicate this line in each function
 
-% Trim conditions and general data
-
-Va0=P.Va0;
-g=P.g;
-
-% Aerosonde UAV Data
-
-% physical parameters of airframe
-
-m=P.m;
-Ixx=P.Ixx;
-Iyy=P.Iyy;
-Izz=P.Izz;
-Ixz=P.Ixz;
-S=P.S;
-b=P.b;
-c=P.c;
-Sprop=P.Sprop;
-e=P.e;
-Omega_max=P.OmegaMax_RPM; % rad/s
-FuelCapacity=P.Fuel_Cap_L; % L
-
-% Longitudinal
-
-Cl0=P.C_Lo;
-CD0=P.C_Do;
-Cm0=P.C_mo;
-Clalpha=P.C_Lalpha;
-CDalpha=P.C_Dalpha;
-Cmalpha=P.C_malpha;
-Clq=P.C_Lq;
-CDq=P.C_Dq;
-Cmq=P.C_mq;
-Cldelta_e=P.C_Ldelta_e;
-CDdelta_e=P.C_Ddelta_e_;
-Cmdelta_e=P.C_mdelta_e_;
-epsilon=P.C_Epsilon;
-
-% Lateral
-
-CY0=P.C_Yo;
-Cl0=P.C_lo;
-Cn0=P.C_no;
-CYbeta=P.C_Ybeta;
-Clbeta=P.C_lbeta;
-Cnbeta=P.C_nbeta;
-CYp=P.C_Yp;
-Clp=P.C_lp;
-Cnp=P.C_np;
-CYr=P.C_Yr;
-Clr=P.C_lr;
-Cnr=P.C_nr;
-CYdelta_a=P.C_Ydelta_a;
-Cldelta_a=P.C_ldelta_a;
-Cndelta_a=P.C_ndelta_a;
-CYdelta_r=P.C_Ydelta_r;
-Cldelta_r=P.C_ldelta_r;
-Cndelta_r=P.C_ndelta_r;
-
 % compute inertial constants
-
-K = Ixx*Izz-Ixz^2;
-k1 = Ixz*(Ixx-Iyy+Izz)/K;
-k2 = (Izz*(Izz-Iyy)+Ixz^2)/K;
-k3 = Izz/K;
-k4 = Ixz/K;
-k5 = (Izz-Ixx)/Iyy;
-k6 = Ixz/Iyy;
-k7 = ((Ixx-Iyy)*Ixx+Ixz^2)/K;
-k8 = Ixx/K;
+K = ;
+k1 = ;
+k2 = ;
+k3 = ;
+k4 = ;
+k5 = ;
+k6 = ;
+k7 = ;
+k8 = ;
 
 % map states and inputs
-
 pn    = block.ContStates.Data(1);
 pe    = block.ContStates.Data(2);
 pd    = block.ContStates.Data(3);
@@ -347,146 +285,47 @@ delta_a = block.InputPort(1).Data(2)*pi/180 ; % converted inputs to radians
 delta_r = block.InputPort(1).Data(3)*pi/180 ; % converted inputs to radians
 delta_t = block.InputPort(1).Data(4);
 
-%calculate density of air
-
-Altitude=-pd;
-
-if Altitude < 11000
-    % Use this if below 11km (in the troposphere)
-    Temperature = 15.04 - .00649 * Altitude;
-    Pressure = 101.29 * [(Temperature + 273.1)/288.08]^5.256;       
-else
-    % Use this if between 11km and 25km (entering lower stratosphere)
-    Temperature = -56.46;
-    Pressure = 22.65 * exp(1.73 - .000157 * Temperature);
-end
-
-rho = Pressure / (0.2869 * (Temperature + 273.1));
-% Air Data
-
-Va = sqrt(u^2+v^2+w^2);
-alpha = atan(w/u);
-beta = asin(v/Va);
+% Air Data 
+Va = ;
+alpha = ;
+beta = ;
 
 % rotation matrix
 
-C_bv=[cos(theta)*cos(psi) cos(theta)*sin(psi) -sin(theta);...
-    sin(phi)*sin(theta)*cos(psi)-cos(phi)*sin(psi) sin(phi)*sin(theta)*sin(psi)+cos(phi)*cos(psi) sin(phi)*cos(theta);...
-    cos(phi)*sin(theta)*cos(psi)+sin(phi)*sin(psi) cos(phi)*sin(theta)*sin(psi)-sin(phi)*cos(psi) cos(phi)*cos(theta)];
-
-% aerodynamic coefficients 
-
+% Aerodynamic Coefficients 
 % compute the nondimensional aerodynamic coefficients here
 
 % aerodynamic forces and moments
-
 % compute the aerodynamic forces and moments here
 
-% Longitudinal aerodynamic forces and moments
-
-F_lift=(1/2)*rho*Va^2*S*(Cl0+Clalpha*alpha+Clq*(c/(2*Va))*q+Cldelta_e*delta_e);
-F_drag=(1/2)*rho*Va^2*S*(CD0+CDalpha*alpha+CDq*(c/(2*Va))*q+CDdelta_e*delta_e);
-
-fx=-F_drag*cos(alpha)+F_lift*sin(alpha);
-fz=-F_drag*sin(alpha)-F_lift*cos(alpha);
-
-M=(1/2)*rho*Va^2*S*c*(Cm0+Cmalpha*alpha+Cmq*(c/(2*Va))*q+Cmdelta_e*delta_e);
-
-% Lateral aerodynamic forces and moments
-
-fy=(1/2)*rho*Va^2*S*(CY0+CYbeta*beta+CYp*(b/(2*Va))*p+CYr*(b/(2*Va))*r+CYdelta_a*delta_e+CYdelta_r*delta_r)
-l=(1/2)*rho*Va^2*S*b*(Cl0+Clbeta*beta+Clp*(b/(2*Va))*p+Clr*(b/(2*Va))*r+Cldelta_a*delta_e+Cldelta_r*delta_r);
-n=(1/2)*rho*Va^2*S*b*(Cn0+Cnbeta*beta+Cnp*(b/(2*Va))*p+Cnr*(b/(2*Va))*r+Cndelta_a*delta_a+Cndelta_r*delta_r);
-
 % propulsion forces and moments
-
 % compute the propulsion forces and moments here
 
-Velocities = Va;
-
-Prop_R = sqrt(P.Sprop/pi);
-Prop_diam = 2*Prop_R;
-
-Fuel_density = (775.0 + 840.0) / 2; % density of Jet A-1 fuel (kg/m^3)
-%https://code7700.com/fuel_density.htm
-
-Fuel_L = P.Fuel_Cap_L /2 ; % amount of fuel in L -> half a tank
-W_fuel_half = (Fuel_L/1000)*Fuel_density*P.gravity; % weight of gas in N
-
-Weight = (P.m * P.gravity) + W_fuel_half;
-
-syms J_variable CT_variable 'real'
-
-CT_J_Curve = fit(P.Props{3}(:,1), P.Props{3}(:,2),'poly2');
-CT_J_Curve_f(J_variable) = CT_J_Curve.p1*(J_variable)^2 + CT_J_Curve.p2*(J_variable) + CT_J_Curve.p3;
-
-CP_J_Curve = fit(P.Props{3}(:,1), P.Props{3}(:,3),'exp2');
-CP_J_Curve_f(J_variable) = CP_J_Curve.a*exp(CP_J_Curve.b*J_variable) + CP_J_Curve.c*exp(CP_J_Curve.d*J_variable);
-
-AR = P.b/P.c;
-K = 1 / (pi*P.e*AR);
-
-q = (1/2)*rho*(Velocities)^2;
-CL = Weight / (q*P.S);
-CD = P.C_Do + K*CL^2;
-Thrust_Req = Weight / (CL / CD);
-
-eqn1 = [CT_variable == CT_J_Curve_f(J_variable)];
-eqn2 = [CT_variable == Thrust_Req / (rho * (Velocities / (Prop_diam * (J_variable)))^2 * Prop_diam^4)];
-eqns = [eqn1(1) eqn2(1)];
-vars = [CT_variable J_variable]; 
-
-[Init_Ct_temp, Init_J_temp] = vpasolve(eqns,vars,[0.05; 0.5]);
-Init_J = double(max(Init_J_temp));
-CT = double(max(Init_Ct_temp));
-
-Rev_sec = Velocities / (Prop_diam * Init_J);
-RPM = Rev_sec*60; 
-
-CP = CP_J_Curve_f(Init_J);
-Power = double(CP * rho*Rev_sec^3*Prop_diam^5);
-
-fp=CT*rho*Rev_sec^2*Prop_diam^4;
-
-CQ=CP/(2*pi);
-Mp=CQ*rho*Rev_sec^2*Prop_diam^5;
-
 % gravity
-
 % compute the gravitational forces here
-
-fg_v=[0; 0; m*g];
-fg_b=C_bv*fg_v;
 
 % total forces and moments (body frame)
 
-f_total=fg_b+[fx; fy; fz]+[fp; 0; 0]
-
-M_total=[l; m; n]+[Mp; 0; 0];
-
 % state derivatives
-
 % the full aircraft dynamics model is computed here
+pdot = ;
+pndot = ;
+pedot = ;
+pddot = ;
 
-%pdot = ;    % extra?
-pndot = (cos(theta)*cos(psi))*u+(sin(phi)*sin(theta)*cos(psi)-cos(phi)*sin(psi))*v+(cos(phi)*sin(theta)*cos(psi)+sin(phi)*sin(psi))*w;
-pedot = (cos(theta)*sin(psi))*u+(sin(phi)*sin(theta)*sin(psi)+cos(phi)*cos(psi))*v+(cos(phi)*sin(theta)*sin(psi)-sin(phi)*cos(psi))*w;
-pddot = -u*sin(theta)+v*sin(phi)*cos(theta)+w*cos(phi)*cos(theta);
+udot = ;
+vdot = ;
+wdot = ;
 
-udot = r*v-q*w+(1/m)*f_total(1);
-vdot = p*w-r*u+(1/m)*f_total(2);
-wdot = q*u-p*v+(1/m)*f_total(3);
+phidot = ;
+thetadot = ;
+psidot = ;
 
-phidot = p+q*sin(phi)*tan(theta)+r*cos(phi)*tan(theta);
-thetadot = q*cos(phi)-r*sin(phi);
-psidot = q*sin(phi)*sec(theta)+r*cos(phi)*tan(theta);
-
-pdot = k1*p*q-k2*q*r+k3*l+k4*n;
-qdot = k5*p*r-k6*(p^2-r^2)+(1/Iyy)*m;
-rdot = k7*p*q-k1*q*r+k4*l+k8*n;
+pdot = ;
+qdot = ;
+rdot = ;
 
 % map derivatives
-
 block.Derivatives.Data(1) = pndot;
 block.Derivatives.Data(2) = pedot;
 block.Derivatives.Data(3) = pddot;
